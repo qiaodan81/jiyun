f567fcf25bf485cf108ebd71db0b102e128f689a
乔joe <2372712751@qq.com>
2020-12-10 10:30:12 +0800

    就以自己的业务来在包下创建编写DTO

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/dto/TestDTO.java
@@ -0,0 +1,18 @@
+package com.ruoyi.system.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author 乔总专用
+ * description:
+ * path: ruoyi-com.ruoyi.system.dto-TestDTO
+ * date: 2020/12/10 10:28
+ * version: 02.06
+ * To change this template use File | Settings | File Templates.
+ *
+ * 就以自己的业务来编写DTO
+ */
+
+public class TestDTO {
+
+}
